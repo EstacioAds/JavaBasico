3d0bbeed98d0c8ac87b77dd48fc40be98924f9fd
Frâncila Weidt Neiva <fran.weidt@gmail.com>
2020-08-27 17:19:43 -0300

    Starting Code

---
+++ JavaBasico/src/javabasico/JavaBasico.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javabasico;
+
+/**
+ *
+ * @author franw
+ */
+public class JavaBasico {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        boolean isSnowing = false;
+        boolean isRaining = true;
+        double temperature = 60.0;
+        // TODO: print "Vamos ficar em casa." se está chovendo, nevando ou
+        // abaixo de 50 graus e imprima "Vamos sair!" caso contrário.
+    }
+    
+}
